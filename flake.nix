9111bc47935f7105a1cdccfef7de2bf7bfd3be33
EzT32 <theodor.berghansen@icloud.com>
2026-06-13 23:09:37 +0200

    Update root flake

--- flake.nix
+++ flake.nix
@@ -19,44 +19,12 @@
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-      nixos-hardware,
-      ...
-    }:
-    let
-      system = "x86_64-linux";
-    in
-    {
-      # Remember to update to 'nixfmt' after transition fase.
-      formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt-tree;
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
 
-      nixosConfigurations = {
-        laptop = nixpkgs.lib.nixosSystem {
-          # inherit system;
-          modules = [
-            ./hosts/laptop
-            ./modules
-            ./overlays
-            home-manager.nixosModules.home-manager
-            nixos-hardware.nixosModules.lenovo-thinkpad-e14-amd
-          ];
-          specialArgs = { inherit system self; };
-        };
+      imports = [ (inputs.import-tree ./modules) ];
 
-        desktop = nixpkgs.lib.nixosSystem {
-          # inherit system;
-          modules = [
-            ./hosts/desktop
-            ./modules
-            ./overlays
-            home-manager.nixosModules.home-manager
-          ];
-
-          specialArgs = { inherit system self; };
-        };
-      };
+      flake.formatter.x86_64-linux = inputs.nixpkgs.legacyPackages.x86_64-linux.nixfmt-tree;
     };
 }
