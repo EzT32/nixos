994a7c9c942155b50c5b8270f80a5369ff142a88
EzT32 <theodor.berghansen@icloud.com>
2026-06-24 05:10:56 +0200

    offload home-manager configuration over to den

--- hosts/entries.nix
+++ hosts/entries.nix
@@ -1,7 +1,9 @@
 # hosts/entries.nix
-{ inputs, ... }:
+{ inputs, lib, ... }:
 {
   imports = [ inputs.den.flakeModule ];
+
+  den.schema.user.classes = lib.mkDefault [ "homeManager" ];
 
   # Declare entities to say "these two things exist".
   den.hosts.x86_64-linux.desktop.users.ezt = { };
