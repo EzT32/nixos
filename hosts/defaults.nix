ddaaea1fd66fdf340bb1ebc6dffd96d6e0045893
EzT32 <theodor.berghansen@icloud.com>
2026-06-24 03:29:27 +0200

    move environment sessionVariables into defaults.nix

--- hosts/defaults.nix
+++ hosts/defaults.nix
@@ -4,6 +4,15 @@
     nixos = {
       nixpkgs.config.allowUnfree = true;
       system.stateVersion = "26.05";
+
+      environment.sessionvariables = {
+        nixos_ozone_wl = "1";
+        gdk_scale = "1.5";
+        qt_scale_factor = "1";
+
+        wlr_no_hardware_cursors = "1";
+        editor = "nvim";
+      };
     };
 
     homeManager = {
