26d302a04f71113da811efab86caefc963910255
EzT32 <theodor.berghansen@icloud.com>
2026-09-21 20:38:24 +0200

    feat: offload configuration to EzT32/nvim

--- modules/dev/nvim.nix
+++ modules/dev/nvim.nix
@@ -1,88 +1,16 @@
 # modules/dev/nvim.nix
-{ inputs, ... }: {
+{ inputs, ... }:
+{
   den.aspects.nvim = {
     homeManager = { pkgs, ... }: {
-      programs.neovim = {
-        enable = true;
-
-        viAlias = true;
-        vimAlias = true;
-        vimdiffAlias = true;
-
-        extraPackages = with pkgs; [
-          lua-language-server
-          stylua
-          luaPackages.luacheck
-
-          nil
-          statix
-
-          wl-clipboard
-
-          vhdl-ls
-          ghdl
-
-          asm-lsp
-          asmfmt
-
-          pyright
-          black
-
-          clang-tools
-
-          jdt-language-server
-        ];
-
-        plugins = with pkgs.vimPlugins; [
-          nvim-lspconfig
-          (nvim-treesitter.withPlugins (
-            p: with p; [
-              lua
-              nix
-              bash
-              vhdl
-              asm
-              python
-              c
-              java
-            ]
-          ))
-
-          telescope-nvim
-          plenary-nvim
-
-          neodev-nvim
-
-          gruvbox-nvim
-          lualine-nvim
-
-          indent-blankline-nvim
-          comment-nvim
-
-          conform-nvim
-
-          nvim-cmp
-          cmp-nvim-lsp
-          cmp-buffer
-          cmp-path
-          luasnip
-          cmp_luasnip
-        ];
-      };
-
-      home.packages = with pkgs; [
-        nixfmt
-      ];
-
-      xdg.configFile."nvim/init.lua".source = "${inputs.nvim-config}/init.lua";
-      xdg.configFile."nvim/lua".source = "${inputs.nvim-config}/lua";
+      home.packages = [
+        inputs.nvim-config.packages.${pkgs.system}.default
+      ]
+      ++ (with pkgs; [ nixfmt ]);
     };
 
     nixos = { pkgs, ... }: {
-      # TODO: add support for more filetypes
-      environment.systemPackages = with pkgs; [
-        nixfmt-tree
-      ];
+      environment.systemPackages = with pkgs; [ nixfmt-tree ];
     };
   };
 }
